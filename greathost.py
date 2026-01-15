edd08180ae54fd983bf0f5348218f2fb23165840
zhangbin0301 <zhangbin0301@qq.com>
2026-01-15 16:24:15 +0800

    Update greathost.py

--- greathost.py
+++ greathost.py
@@ -17,7 +17,7 @@ EMAIL = os.getenv("GREATHOST_EMAIL") or ""
 PASSWORD = os.getenv("GREATHOST_PASSWORD") or ""
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or ""
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID") or ""
-# sock5代码，不需要留空值
+# sock5代码，不需要留空值 62行要填上IP头
 PROXY_URL = os.getenv("PROXY_UR") or ""
 
 def send_telegram(msg_type_or_text, error_msg=None):    
